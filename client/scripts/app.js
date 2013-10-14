08d773615db03da5f06105a9086f299e574f29d0
stangah <pstanger@gmail.com>
2013-10-14 11:28:27 -0700

    Stashing old work

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,19 @@
-// YOUR CODE HERE:
+var _lastMsgTime = 0;
+
+var retrieve = function() {
+  $.get('https://api.parse.com/1/classes/chatterbox', function(data) {
+    _(data.results).each(function(obj) {
+      if ( new Date(obj.createdAt) > new Date(_lastMsgTime) ) {
+        _lastMsgTime = new Date(obj.createdAt);
+        $('.messages').append('<span>' + obj.username + ': ' + obj.text + '</span>');
+      }
+    });
+  });
+};
+
+var send = function(string) {
+  console.log( $('.userInput').val() );
+  $('.userInput').val();
+};
+
+setInterval(retrieve, 1000);
